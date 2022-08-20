537e9653d76b663cfafb2501b37b25b8c131cdff
Carranya <k.gong82@gmail.com>
2022-08-20 11:35:54 +0200

    keep old home.php

---
+++ public/home_old.php
@@ -0,0 +1,12 @@
+
+<div class="flex" id="mainabove">
+    <div class="w-11" id="next">
+        <h1 class="text-4xl font-bold text-center m-10 block">Herzlich Willkommen</h1>
+        <p>Nächster Termin: </p>
+    </div>
+
+    <div class="bg-red-100" id="special">
+        <h1 class="text-3xl font-bold text-center m-10 block">Spezial Event:</h1>
+        <p class="text.left">Roger's Karaokezebra</p>
+    </div>
+</div>
