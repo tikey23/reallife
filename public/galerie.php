f56e5ef9e1eb28a4b1a396d32295e28919f79e8d
Carranya <k.gong82@gmail.com>
2022-08-28 20:52:33 +0200

    update

--- public/galerie.php
+++ public/galerie.php
@@ -17,7 +17,7 @@
 <button name='thema' value='unterwasser'><div class="text-xl" style="width: 220px; height: 300px; padding: 10px; margin: 10px; border-radius: 20px; background-color: #8b5cf6; /* bg-viloet-500 */; border: 1px solid black">
     <img src="/img/galerie/unterwasser_abend/3.jpg" style="height: 200px; width: 100%; object-fit: cover; object-position: top center; border-radius: 10px;"></img>
     <br>
-    Unterwasser Abend
+    <p>Unterwasser Abend</p>
  
 </div></button>
 </form>
