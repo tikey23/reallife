f3ca1f6d5a56c14cdd3e96fddecd3527051dc12b
Carranya <k.gong82@gmail.com>
2022-08-30 18:12:04 +0200

    updates

--- public/test2.php
+++ public/test2.php
@@ -1,35 +1,16 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="/css/output.css" rel="stylesheet">
-</head>
-<body>
-<style>
-  
+<?php
+//Declare the array
+$flowers = array(
+                "Rose",
+                "Lili",
+                "Jasmine",
+                "Hibiscus",
+                "Tulip",
+                "Sun Flower",
+                "Daffodil",
+                "Daisy");
 
-    
-.reihe{
-	width: 50px;
-	height: 50px;
-	background-color: #BEBEBE;
-}
-
-.reihe img{
-	position: absolute;
-	clip: rect(0px 50px 50px 0px);
-	overflow: hidden;
-}
-
-
-</style>
-    
-
-<div class="reihe">
-    <img src="/img/galerie/unterwasser_abend/1.jpg"></img>
-</div>
-
-
-</body>
-</html>
+unset($flowers[1]);
+echo "The array is:\n";
+print_r($flowers);
+?>
