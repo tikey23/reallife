c689e0fa8f7932bffc32cbf6e2ff08f7080092ad
Carranya <k.gong82@gmail.com>
2022-09-08 20:31:56 +0200

    #509 "Sicherheitslücke" fixed

--- public/galeriebilder.php
+++ public/galeriebilder.php
@@ -12,7 +12,6 @@ if(isset($_POST['folder'])) {
 
 $folder = $_SESSION['folder'];
 global $con;
-// $con = new mysqli("", "root", "", "reallife");
 
 if($folder == "neu") {
     $_SESSION['folder'] = $_POST['titel'];
@@ -22,34 +21,8 @@ if($folder == "neu") {
 }
 
 if(isset($_SESSION['password'])) {
-
     addpic();
-    
-    if(isset($_POST['picupload'])) {
-        
-        $target_dir = "img/galerie/" . $folder . "/";
-        $uploadfile = $target_dir . basename($_FILES['fileToUpload']['name']);
-        if (file_exists($uploadfile)) {
-            echo "<br><p class='font-bold'>Fehler! Bild existiert bereits im Dateiordner!</p>";
-        } else {
-
-        move_uploaded_file($_FILES['fileToUpload']['tmp_name'], $uploadfile);
-        $file = basename($_FILES['fileToUpload']['name']);
-        
-
-        $sql = "INSERT INTO gallery (folder, dateiname) values 
-        ('" . $_SESSION['folder'] . "', '$file')";
-        $con->query($sql);
-        }
-
-        $resul = $con->query("SELECT * FROM gallerycategory WHERE folder = '" . $_SESSION['folder'] . "'");
-        $titeldata = $resul->fetch_assoc();
-
-        if ($titeldata['titel'] == "") {
-            echo $file . "<br>";
-            $con->query("UPDATE gallerycategory SET titel = '$file' WHERE folder = '" . $_SESSION['folder'] . "'");
-        }
-    }
+    uploadpic($folder);
 }
 
 showpics($folder);
