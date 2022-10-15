35f8ff4fcc01c0116697bba76d9093fa5ff388d1
Carranya <k.gong82@gmail.com>
2022-10-15 11:46:11 +0200

    add map an Index

--- public_html/pages/home.php
+++ public_html/pages/home.php
@@ -16,22 +16,26 @@
 	<br><br>
 	</div>
 
-	<p class='text-center font-bold'><u>Adresse:</u><br>
-	Bitwäscherei<br>
-	Neue Hard 12<br>
-	8005 Zürich</p>
+	<div class="flex justify-center items-center mt-5">
+		<div class='text-left font-bold block mr-5'>
+			<u>Adresse:</u><br>
+			Bitwäscherei<br>
+			Neue Hard 12<br>
+			8005 Zürich
+		</div>
 
-	<!-- <center>
-		<div class="mapouter">
-			<div class="gmap_canvas">
-				<iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Bitw%C3%A4scherei,%20Neue%20Hard%2012,%208005%20Z%C3%BCrich&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
-				<a href="https://123movies-to.org"></a>
-				<br>
-					<style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style>
-					<a href="https://www.embedgooglemap.net">google iframe</a>
-					<style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+		<div class="hidden sm:contents">
+			<div class="mapouter">
+				<div class="gmap_canvas">
+					<iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Bitw%C3%A4scherei,%20Neue%20Hard%2012,%208005%20Z%C3%BCrich&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+					<a href="https://123movies-to.org"></a>
+					<br>
+						<style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}</style>
+						<a href="https://www.embedgooglemap.net">google iframe</a>
+						<style>.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style>
+				</div>
 			</div>
 		</div>
-	</center> -->
+	</div>
 
 </div>
