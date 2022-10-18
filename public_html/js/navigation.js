994f73a9fbb78f0ae9c96e776bfec4e7f3a93c87
Carranya <k.gong82@gmail.com>
2022-10-18 16:07:40 +0200

    created hamburger button for S-Phone

--- public_html/js/navigation.js
+++ public_html/js/navigation.js
@@ -6,10 +6,13 @@ window.onload = function(){
 function openNavi(){
     let navi = document.getElementById("navigation");
     navi.style.display = "block";
+    document.getElementById("main").style.opacity = 0.3;
+    
 }
 
 function closeNavi(){
     let navi = document.getElementById("navigation");
     navi.style.display = "none";
+    document.getElementById("main").style.opacity = 1;
 }
 
