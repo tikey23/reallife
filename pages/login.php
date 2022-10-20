9df09faefe848635574a9cdfefa0ec420f38a0b5
Carranya <k.gong82@gmail.com>
2022-10-20 17:20:45 +0200

    fixed login bugs

--- pages/login.php
+++ pages/login.php
@@ -2,30 +2,33 @@
 
 use \Rl\Models\Member;
 
-if (!isset($_SESSION['username'])) {
+if (isset($_POST['username'])) {
 	$username = $_POST['username'];
 	$pwd = $_POST['pwd'];
-
 	$member = findOneByColumn(Member::class, 0, "membername", $username);
-} else {
-	$username = $_SESSION['username'];
-	$pwd = $_SESSION['pwd'];
-	$member = findOneByColumn(Member::class, 0, "membername", $_SESSION['username']);
-}
+		
+	if ($member == "error") {
+		echo "Anmeldedaten sind nicht korrekt";
+	} else {
+	
+		if (password_verify($pwd, $member->pwd)) {
+			if ($member->memberfunction == "Admin") {
+				$_SESSION['admin'] = true;
+				$_SESSION['leader'] = true;
+				$_SESSION['helper'] = true;
+			} elseif ($member->memberfunction == "Leiter") {
+				$_SESSION['leader'] = TRUE;
+				$_SESSION['helper'] = TRUE;
+			} else {
+				$_SESSION['helper'] = TRUE;
+			}
+			$_SESSION['username'] = $username;
 
-if ($member == "error") {
-	echo "Anmeldedaten sind nicht korrekt";
-} else {
-	if (password_verify($pwd, $member->pwd)) {
-
-		$_SESSION['username'] = $username;
-		$_SESSION['pwd'] = $pwd;
-		if (!isset($_SESSION['refresh'])) {
 			header("location:/index.php?page=userArea");
 			exit;
+		} else {
+			echo "Anmeldung fehlgeschlagen";
 		}
-	} else {
-		echo "Anmeldung fehlgeschlagen";
 	}
 }
 
