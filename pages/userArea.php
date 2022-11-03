5d922d621887653f539554d1bd40035e5827ccd2
Carranya <k.gong82@gmail.com>
2022-11-03 21:29:38 +0100

    added "Your next deployment" message on UserArea

--- pages/userArea.php
+++ pages/userArea.php
@@ -7,7 +7,7 @@ if(!$_SESSION['username']) die("buuuuh");
 $username = $_SESSION['username'];
 $member = findOneByColumn(Member::class, 0, "membername", $_SESSION['username']);
 if($member == NULL) die("not allowed");
-$events = findAll(Event::class);
+$events = listActualEvent();
 
 echo $twig->render('user/userMain.twig', ["member" => $member, "events" => $events]);
 
