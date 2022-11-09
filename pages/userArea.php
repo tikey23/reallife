27a3571bd08d6d302565cc35c55f5385c5103b02
Carranya <k.gong82@gmail.com>
2022-11-09 20:06:35 +0100

    set change Password function in UserArea

--- pages/userArea.php
+++ pages/userArea.php
@@ -9,5 +9,18 @@ $member = findOneByColumn(Member::class, 0, "membername", $_SESSION['username'])
 if($member == NULL) die("not allowed");
 $events = listActualEvent();
 
+if(isset($_POST["modifyPwd"])){
+    $oldpwd = $_POST['oldpwd'];
+    if (password_verify($oldpwd, $member->pwd)) {
+        $newPwd = $_POST['memberNewPwd'];
+        $memberNewPwd = password_hash($newPwd, PASSWORD_DEFAULT);
+        $member->pwd = $memberNewPwd;
+        $member->save();
+        echo "Passwort angepasst.";
+    } else {
+        echo "Altes Passwort falsch. Passwort wurde nicht geändert.";
+    }
+}
+
 echo $twig->render('user/userMain.twig', ["member" => $member, "events" => $events]);
 
