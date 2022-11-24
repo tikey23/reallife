61ecb75f69536137e8905a1e6c0387c62520a57e
Carranya <k.gong82@gmail.com>
2022-11-24 22:58:56 +0100

    create introduction page #539

--- pages/home.php
+++ pages/home.php
@@ -1,5 +1,6 @@
 <div class="text-center block lg:flex justify-center">
 	<?php
+	echo $twig->render('home/home.twig');
 	$specialEvents = showSpecialEvents();
 	echo $twig->render('home/specialEvents.twig', ["events" => $specialEvents]);
 	?>
